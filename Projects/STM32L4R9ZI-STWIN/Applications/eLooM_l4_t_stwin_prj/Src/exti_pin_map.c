c135b56378ab756403b376fd03d1ab95c6f1cc90
Stefano Oliveri <software@stf12.net>
2021-11-28 13:20:17 +0100

    L4_t: added L4 template project.
    Developer board: STWIN1b.

---
+++ Projects/STM32L4R9ZI-STWIN/Applications/eLooM_l4_t_stwin_prj/Src/exti_pin_map.c
@@ -0,0 +1,33 @@
+/**
+ ******************************************************************************
+ * @file    exti_pin_map.c
+ * @author  STMicroelectronics - AIS - MCD Team
+ * @version V1.0.0
+ * @date    15-September-2021
+ * @brief   Application level file. It defines the PIN to callback
+ *          mapping function for the external interrupt.
+ *
+ * TODO - insert here the file description
+ *
+ *********************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2021 STMicroelectronics
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file in
+ * the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *********************************************************************************
+ */
+
+#include "drivers/EXTIPinMap.h"
+
+// Forward function declaration.
+void Def_EXTI_Callback(uint16_t pin) {};
+void HW_PB_EXTI_Callback(uint16_t pin);
+
+EXTI_BEGIN_P2F_MAP()
+  EXTI_P2F_MAP_ENTRY(GPIO_PIN_0, HW_PB_EXTI_Callback)
+
+EXTI_END_P2F_MAP()
